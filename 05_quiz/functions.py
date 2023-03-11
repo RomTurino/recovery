b8e1c01f2a87af323cf1b4f3f9485d25b5b8832e
RomTurino <73258691+RomTurino@users.noreply.github.com>
2023-03-11 19:30:35 +0300

    Добавил вопросы в файл

---
+++ 05_quiz/functions.py
@@ -0,0 +1,49 @@
+import csv
+from telegram.ext import CallbackContext, ConversationHandler
+from telegram import (
+    Update,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove
+)
+import random
+
+GO = 'Поехали'
+GAME = 1
+
+def start(update: Update, context: CallbackContext):
+    mark_up = [[GO]]
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=mark_up,
+        resize_keyboard=True,
+        one_time_keyboard=True,
+        input_field_placeholder=f'Нажми на кнопку "{GO}", поиграем!'
+    )
+    # update.message.reply_sticker(START_STICKER)
+    update.message.reply_text(
+        'Добро пожаловать в викторину! Выбирайте правильный ответ')
+    update.message.reply_text(
+        f'Чтобы начать, нажми на "{GO}"', reply_markup=keyboard)
+    return GAME
+
+
+def game(update: Update, context: CallbackContext):
+    questions = read_csv()
+    random.shuffle(questions)
+
+def end(update: Update, context: CallbackContext):  # точка выхода
+    name = context.user_data['имя']
+    update.message.reply_text(f"Значит, ты выбрал конец, {name}. Если хотите начать заново, нажмите /start")
+    return ConversationHandler.END
+
+def read_csv():
+    with open('05_quiz\вопросы.csv', 'r', encoding='utf-8') as file:
+        quest = list(csv.reader(file, delimiter='|'))
+        return quest
+
+
+def write_csv():
+    with open('05_quiz\вопросы.csv', 'a', encoding='utf-8') as file:
+        worker = csv.writer(file, delimiter='|',
+                            lineterminator='\n')  # \n - это перенос
+        worker.writerow(['Какая столица Татарстана?',
+                        'Казань', 'Астана', 'Елабуга', 'Челны'])
---
+++ 05_quiz/main.py
@@ -0,0 +1,28 @@
+from config import TOKEN
+from telegram.ext import (
+    Updater,
+    Filters,
+    MessageHandler,
+    CommandHandler,
+    ConversationHandler
+)
+from functions import *
+
+
+updater = Updater(TOKEN)
+dispatcher = updater.dispatcher
+
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler('start', start)], # точка входа в разговор
+    states={ # этапы разговора
+           
+        },
+    fallbacks=[CommandHandler('end', end)] # точка выхода из разговора
+)
+
+
+dispatcher.add_handler(conv_handler)
+
+print('Сервер запущен!')
+updater.start_polling()
+updater.idle()  # ctrl + C
